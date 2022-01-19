cf2e45f4b186df87caee841834678584e2c312a8
Landon Koterba <97569192+lkoterba@users.noreply.github.com>
2022-01-19 16:30:13 -0500

    git-lab-program.cc
    @Jrodmath done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Landon Koterba
+ *        Date: 1-19-2022
+ * Description: This will display "Hello Git!!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main 
